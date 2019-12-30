69bee1500873911c451503623c30b376ca44f1cc
al7771998 <b05902018@ntu.edu.tw>
2019-12-30 14:26:46 +0800

    add databaseUtil

--- src/main.java
+++ src/main.java
@@ -18,6 +18,23 @@ import java.text.SimpleDateFormat;
 public class main {
 	public static void main(String[] args) 
 	{
-		
+		databaseUtil.buildConnection();
+//		databaseUtil.initDatabase();
+		ReadHotelList();
+		HotelPreference program = new HotelPreference();
+		program.setVisible(true);
+	}
+	public static void ReadHotelList()  {
+		try (Reader reader = new InputStreamReader(main.class.getResourceAsStream("HotelList.json"), "big5")) {
+			// try (BufferedReader reader = new BufferedReader(new FileReader(file)) {
+			Gson gson = new GsonBuilder().create();
+			HotelList = gson.fromJson(reader, Hotel[].class);
+			for (Hotel h : HotelList)
+				h.init();
+			for (Hotel h : HotelList)
+				System.out.println(h);
+		} catch (Exception e) {
+			System.out.println("cannot find the file.");
+		}
 	}
 }
